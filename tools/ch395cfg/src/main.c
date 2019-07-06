3fcbd82ae2840b36621cdc0d84926b2127459e6e
jede <jede@oric.org>
2019-07-06 10:42:00 +0200

    update tool

--- tools/ch395cfg/src/main.c
+++ tools/ch395cfg/src/main.c
@@ -1,5 +1,6 @@
 #include "ch395.h"
 #include <string.h>
+#include <telestrat.h>
 
 #include <conio.h>
 
@@ -8,18 +9,30 @@ void menu () {
 }
 
 char *firmware_version_str="CH395 firmware version : ";
+char *ipaddress_str="IP address : ";
 
 int main() {
+    unsigned char version;
+    clrscr();
 
-unsigned char version;
-clrscr();
-
-version=ch395_get_ic_ver();
-textcolor(COLOR_MAGENTA);
-cputsxy(0,0,"CH395 Config tool");
-cputsxy(0,2,firmware_version_str);
-//cputsxy(strlen(firmware_version_str)+2,2,ch395_get_ic_ver());
-
+    //version=ch395_get_ic_ver();
+    bgcolor (COLOR_BLUE);
+    textcolor(COLOR_WHITE);
+    gotoxy(2,1);
+    cputs("+---------------------------------+");
+    gotoxy(2,2);
+   
+    cputs("|          CH395 Config tool      |");
+    gotoxy(2,3);
+    cputs("+---------------------------------+");
+    gotoxy(2,5);
+    bgcolor (COLOR_MAGENTA);
+    cputs(firmware_version_str);
+    gotoxy(2,7);
+    cputs(ipaddress_str);
+    
+    //cputsxy(0,2,firmware_version_str);
+    //cputsxy(strlen(firmware_version_str)+2,2,ch395_get_ic_ver());
 
 return 0;
 }
