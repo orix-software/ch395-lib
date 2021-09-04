ae0f5101e0e42be4ce73a2867ff42875877a88f4
jede <jede@oric.org>
2021-09-05 00:11:37 +0200

    fix assembly mode for ip_addr_sn

--- src/6502/_ch395_set_ip_addr_sn.s
+++ src/6502/_ch395_set_ip_addr_sn.s
@@ -2,22 +2,40 @@
     .include "ch395.inc"
 .endif    
 
-.importzp ptr1
+.ifndef ptr1
+    .importzp ptr1
+.endif    
 
-.import popax
+.ifndef      FROM_ASSEMBLY
+    .import popax
+.endif    
 
 .export _ch395_set_ip_addr_sn
 
 ; void ch395_set_ip_addr_sn(unsigned char ip_addr[],unsigned char ID_SOCKET)
 
 .proc _ch395_set_ip_addr_sn
+
+.ifdef      FROM_ASSEMBLY
+    sty     ptr1
+    stx     ptr1+1
+.else
+  
+
+.endif    
+
     ldx     #CH395_SET_IP_ADDR_SN
     stx     CH395_COMMAND_PORT
     sta     CH395_DATA_PORT ; Send socket id
+
+.ifdef      FROM_ASSEMBLY
+.else
     jsr     popax ; get ip_addr
-    
     sta     ptr1
     stx     ptr1+1
+.endif    
+    
+    
     ; send IP
     ldy     #$00
 @loop:
