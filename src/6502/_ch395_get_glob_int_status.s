de7115a7290796471506e5cee4410646982abcba
jede <jede@oric.org>
2022-06-02 19:03:50 +0200

    fix bug

--- src/6502/_ch395_get_glob_int_status.s
+++ src/6502/_ch395_get_glob_int_status.s
@@ -1,15 +1,14 @@
 .ifndef CH395_COMMAND_PORT
     .include "ch395.inc"
-.endif    
+.endif
 
 .export _ch395_get_glob_int_status
 
 .proc _ch395_get_glob_int_status
-    
 	lda     #CH395_GET_GLOB_INT_STATUS
     sta     CH395_COMMAND_PORT
 
     lda     CH395_DATA_PORT
-    ldx     CH395_DATA_PORT
+    ;ldx     CH395_DATA_PORT
     rts
 .endproc
