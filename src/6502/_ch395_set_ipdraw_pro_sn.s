b659daae5057fa2af70569e44965e520088e26a3
jede <jede@oric.org>
2022-06-16 00:02:13 +0200

    add set ipdraw_pro_sn

---
+++ src/6502/_ch395_set_ipdraw_pro_sn.s
@@ -0,0 +1,20 @@
+.ifndef CH395_COMMAND_PORT
+    .include "../include/ch395.inc"
+.endif
+
+.proc _ch395_set_ipdraw_pro_sn
+    ; A the socket
+    ; X the mode proto
+
+    ;xWriteCH395Cmd(CMD20_SET_IPRAW_PRO_SN);
+    ;xWriteCH395Data(sockindex);
+    ;xWriteCH395Data(prototype);
+    ;xEndCH395Cmd();
+
+    ldy     #CH395_SET_IPRAW_PRO_SN
+    sty     CH395_COMMAND_PORT
+    sta     CH395_DATA_PORT ; Set socket port
+    stx     CH395_DATA_PORT
+
+    rts
+.endproc
