bf8b5f8fab1109c49df54f6770c29dff3ec63335
jedeoric <jede@oric.org>
2022-12-23 15:36:39 +0100

    remove trailing space

--- src/6502/_ch395_set_fun_para.s
+++ src/6502/_ch395_set_fun_para.s
@@ -10,10 +10,10 @@
     ldy     #CH395_SET_FUN_PARA
     sty     CH395_COMMAND_PORT
     ; Send four bytes
-    sta     CH395_DATA_PORT 
+    sta     CH395_DATA_PORT
     lda     #$00
-    sta     CH395_DATA_PORT 
-    sta     CH395_DATA_PORT 
-    sta     CH395_DATA_PORT 
+    sta     CH395_DATA_PORT
+    sta     CH395_DATA_PORT
+    sta     CH395_DATA_PORT
     rts
 .endproc
