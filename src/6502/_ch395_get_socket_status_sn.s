38f7ceed4393f89350f4422a1937d942bcbb524c
jede <jede@oric.org>
2023-11-18 23:18:20 +0100

    add_doc

--- src/6502/_ch395_get_socket_status_sn.s
+++ src/6502/_ch395_get_socket_status_sn.s
@@ -8,7 +8,7 @@
 ; unsigned int ch395_get_socket_status_sn(unsigned char ID_SOCKET);
 
 .proc _ch395_get_socket_status_sn
-	ldx     #CH395_GET_SOCKET_STATUS_SN
+    ldx     #CH395_GET_SOCKET_STATUS_SN
     stx     CH395_COMMAND_PORT
     sta     CH395_DATA_PORT
     lda     CH395_DATA_PORT
