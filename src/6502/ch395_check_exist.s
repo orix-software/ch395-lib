ae3c58212171e0243d0b2bb4009f8b63b0ec1c36
jede <jede@oric.org>
2024-03-20 22:00:11 +0100

    update

--- src/6502/ch395_check_exist.s
+++ src/6502/ch395_check_exist.s
@@ -3,7 +3,9 @@
 .export ch395_check_exist
 
 .proc ch395_check_exist
-    ;;@brief Checks if ch395 exists
+    ;;@brief This command is used to test the communication interface and working state to check whether CH395 is working properly. This command needs to input 1 byte of data, which can be any data. If CH395 is working properly, the output data of CH395 will be the bitwise reverse of the input data. For example, if the input data is 57H, the output data will be A8H.
+    ;;@modifyA
+    ;;@returnsA Returns '#CH395_DETECTED' if it exists
     ;;@```ca65
     ;;@`  jsr ch395_check_exist
     ;;@`  cmp       #CH395_DETECTED
@@ -11,8 +13,6 @@
     ;;@`  rts
     ;;@`ch395_connected:
     ;;@```
-    ;;@modifyA
-    ;;@returnsA Returns #CH395_DETECTED if it exists
 	lda     #CH395_CHECK_EXIST
     sta     CH395_COMMAND_PORT
     lda     #$55
--- src/6502/ch395_get_ic_ver.s
+++ src/6502/ch395_get_ic_ver.s
@@ -3,7 +3,7 @@
 .export ch395_get_ic_ver
 
 .proc ch395_get_ic_ver
-    ;;@brief Get ch395 firmware version
+    ;;@brief This command is used to get the chip and firmware versions. 1 byte of data returned is the version number, the bit 7 is 0, the bit 6 is 1, and the bits 5-0 are the version number. If the returned value is 41H, remove bits 7 and 6, and the version number will be 01H. It is called chip version in this text
     ;;@modifyA
     ;;@returnsA Version
     ;;@```ca65
@@ -11,7 +11,6 @@
     ;;@`  ; Check A for version
     ;;@`  rts
     ;;@```
-
 	lda     #CH395_GET_IC_VER
     sta     CH395_COMMAND_PORT
     lda     CH395_DATA_PORT
