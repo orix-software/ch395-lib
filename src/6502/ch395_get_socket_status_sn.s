75050ea4c2993341530bef6cba4f5a22bfad72b4
jede <jede@oric.org>
2024-04-11 22:04:22 +0200

    fix

--- src/6502/ch395_get_socket_status_sn.s
+++ src/6502/ch395_get_socket_status_sn.s
@@ -5,23 +5,23 @@
 .proc ch395_get_socket_status_sn
     ;;@brief This command is used to get Socket status. It is necessary to input a 1 byte of Socket index value. CH395 will output a 2-byte status code when receiving this command. The first status code is the status code of Socket. The status code of Socket is defined as follows:
     ;;Code Name
-    ;;00H SOCKET_CLOSED
-    ;;05H SOCKET_OPEN
-    ;;The second status code is TCP status code, which is only meaningful when TCP mode has been on. TCP
-    ;;status code is defined as follows:
-    ;;Code Name Description
-    ;;00H TCP_CLOSED Closed
-    ;;01H TCP_LISTEN Monitoring
-    ;;02H TCP_SYN_SENT SYN sent
-    ;;03H TCP_SYN_RCVD SYN received
-    ;;04H TCP_ESTABLISHED TCP connection established
-    ;;05H TCP_FIN_WAIT_1 The active closing side first sends FIN
-    ;;06H TCP_FIN_WAIT_2 The active closing side receives an ACK from FIN
-    ;;07H TCP_CLOSE_WAIT The passive closing side receives FIN
-    ;;08H TCP_CLOSING Closing
-    ;;09H TCP_LAST_ACK The passive closing side sends FIN
-    ;;0AH TCP_TIME_WAIT 2MLS waiting status
-    ;;TCP status is defined in TCP/IP protocol. Please refer to TCP/IP protocol for the detailed meaning
+    ;;@explain 00H SOCKET_CLOSED
+    ;;@explain 05H SOCKET_OPEN
+    ;;@explain The second status code is TCP status code, which is only meaningful when TCP mode has been on. TCP
+    ;;@explain status code is defined as follows:
+    ;;@explain Code Name Description
+    ;;@explain 00H TCP_CLOSED Closed
+    ;;@explain 01H TCP_LISTEN Monitoring
+    ;;@explain 02H TCP_SYN_SENT SYN sent
+    ;;@explain 03H TCP_SYN_RCVD SYN received
+    ;;@explain 04H TCP_ESTABLISHED TCP connection established
+    ;;@explain 05H TCP_FIN_WAIT_1 The active closing side first sends FIN
+    ;;@explain 06H TCP_FIN_WAIT_2 The active closing side receives an ACK from FIN
+    ;;@explain 07H TCP_CLOSE_WAIT The passive closing side receives FIN
+    ;;@explain 08H TCP_CLOSING Closing
+    ;;@explain 09H TCP_LAST_ACK The passive closing side sends FIN
+    ;;@explain 0AH TCP_TIME_WAIT 2MLS waiting status
+    ;;@explain TCP status is defined in TCP/IP protocol. Please refer to TCP/IP protocol for the detailed meaning
     ;;@inputA Socket id
     ;;@modifyX
     ;;@modifyA
